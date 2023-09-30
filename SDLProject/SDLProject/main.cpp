29c5ec7b8221b766e30174f00d19f0051cf7c38f
Wea3el <wesleyz2004@gmail.com>
2023-09-29 21:24:18 -0400

    fixed rotation and direction, deleted unecessary code

--- SDLProject/SDLProject/main.cpp
+++ SDLProject/SDLProject/main.cpp
@@ -20,9 +20,9 @@
 const int WINDOW_WIDTH  = 640,
           WINDOW_HEIGHT = 480;
 
-const float BG_RED     = 0.0f,
-            BG_BLUE    = 0.0f,
-            BG_GREEN   = 0.0f,
+const float  BG_RED = 0.0f,
+BG_BLUE = 0.0f,
+BG_GREEN = 0.0f,
             BG_OPACITY = 1.0f;
 
 const int VIEWPORT_X = 0,
@@ -42,19 +42,21 @@ const float MINIMUM_COLLISION_DISTANCE = 1.0f;
 
 const float RADIUS = 2.0f;      // radius of circle
 const float ROT_SPEED = 0.01f;  // rotational speed
-const float ROT_ANGLE = glm::radians(5.0f);
+const float ROT_ANGLE = glm::radians(1.5f);
+const float TRAN_VALUE = 0.01f;
 
 float g_angle    = 0.0f;        // current angle accumulated
 float g_x_coords = RADIUS;      // current x-coord
 float g_y_coords = 0.0f;
 
-const float GROWTH_FACTOR = 1.1f;  // grow by 1.0% / frame
+const float GROWTH_FACTOR = 1.01f;  // grow by 1.0% / frame
 const float SHRINK_FACTOR = 0.99f;  // grow by -1.0% / frame
 
 
 SDL_Window* display_window;
 bool game_is_running = true;
 bool is_growing = true;
+int  g_frame_counter   = 0;
 
 ShaderProgram g_program;
 glm::mat4 g_view_matrix, g_model_matrix, g_projection_matrix, other_model_matrix;
@@ -65,16 +67,14 @@ GLuint player_texture_id;
 GLuint other_texture_id;
 
 glm::vec3 player_position = glm::vec3(0.0f, 0.0f, 0.0f);
-glm::vec3 player_movement = glm::vec3(0.2f, 0.2f, 0.0f);
+
 
 glm::vec3 other_position = glm::vec3(0.0f, 0.0f, 0.0f);
-glm::vec3 other_movement = glm::vec3(0.2f, 0.2f, 0.0f);
 
-glm::vec3 player_orientation = glm::vec3(0.0f, 0.0f, 0.0f);
-glm::vec3 player_rotation    = glm::vec3(0.0f, 0.0f, 0.0f);
 
-float player_speed = 1.0f;  // move 1 unit per second
-float other_speed = 1.5f;
+
+float player_speed = 0.1f;  // move 1 unit per second
+float other_speed = 0.05f;
 
 bool g_is_growing = true;
 #define LOG(argument) std::cout << argument << '\n'
@@ -85,29 +85,29 @@ const GLint TEXTURE_BORDER   = 0;   // this value MUST be zero
 
 GLuint load_texture(const char* filepath)
 {
-    // STEP 1: Loading the image file
+    
     int width, height, number_of_components;
     unsigned char* image = stbi_load(filepath, &width, &height, &number_of_components, STBI_rgb_alpha);
-    
+
     if (image == NULL)
     {
         LOG("Unable to load image. Make sure the path is correct.");
         assert(false);
     }
+
     
-    // STEP 2: Generating and binding a texture ID to our image
     GLuint textureID;
     glGenTextures(NUMBER_OF_TEXTURES, &textureID);
     glBindTexture(GL_TEXTURE_2D, textureID);
     glTexImage2D(GL_TEXTURE_2D, LEVEL_OF_DETAIL, GL_RGBA, width, height, TEXTURE_BORDER, GL_RGBA, GL_UNSIGNED_BYTE, image);
+
     
-    // STEP 3: Setting our texture filter parameters
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
     
-    // STEP 4: Releasing our file from memory and returning our texture id
     stbi_image_free(image);
-    
+
     return textureID;
 }
 
@@ -118,36 +118,38 @@ void initialise()
                                       SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
                                       WINDOW_WIDTH, WINDOW_HEIGHT,
                                       SDL_WINDOW_OPENGL);
-    
+
     SDL_GLContext context = SDL_GL_CreateContext(display_window);
     SDL_GL_MakeCurrent(display_window, context);
-    
+
 #ifdef _WINDOWS
     glewInit();
 #endif
-    
+
     glViewport(VIEWPORT_X, VIEWPORT_Y, VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
-    
+
     g_program.load(V_SHADER_PATH, F_SHADER_PATH);
-    
+
+
     g_model_matrix = glm::mat4(1.0f);
+
     other_model_matrix = glm::mat4(1.0f);
     other_model_matrix = glm::translate(other_model_matrix, glm::vec3(1.0f, 1.0f, 0.0f));
-    
-    
-    g_view_matrix = glm::mat4(1.0f);  // Defines the position (location and orientation) of the camera
-    g_projection_matrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);  // Defines the characteristics of your camera, such as clip planes, field of view, projection method etc.
+
+
+    g_view_matrix = glm::mat4(1.0f);
+    g_projection_matrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);
     
     g_program.set_projection_matrix(g_projection_matrix);
     g_program.set_view_matrix(g_view_matrix);
-    
+
     glUseProgram(g_program.get_program_id());
-    
+
     glClearColor(BG_RED, BG_BLUE, BG_GREEN, BG_OPACITY);
-    
+
     player_texture_id = load_texture(PLAYER_SPRITE_FILEPATH);
     other_texture_id = load_texture(OTHER);
-    
+
     // enable blending
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -164,7 +166,7 @@ void process_input()
                 game_is_running = false;
             }
         }
-    
+
 }
 
 /**
@@ -173,38 +175,41 @@ void process_input()
 
 void update()
 {
-    float ticks = (float) SDL_GetTicks() / MILLISECONDS_IN_SECOND; // get the current number of ticks
-    float delta_time = ticks - previous_ticks; // the delta time is the difference from the last frame
-    previous_ticks = ticks;
+
+
     
+    g_frame_counter++;
+    if (g_frame_counter >= 100)
+        {
+            is_growing = !is_growing;
+            g_frame_counter = 0;
+        }
     glm::vec3 scale_vector;
-       
-    scale_vector = glm::vec3(GROWTH_FACTOR, GROWTH_FACTOR, 1.0f);
     
     
-    // Add direction * units per second * elapsed time
-    player_position += player_movement * player_speed * delta_time;
+    scale_vector = glm::vec3(is_growing ? GROWTH_FACTOR : SHRINK_FACTOR,
+                                 is_growing ? GROWTH_FACTOR : SHRINK_FACTOR,
+                                 1.0f);
+    g_model_matrix =glm::scale(g_model_matrix, scale_vector);
+
     
-    other_position += other_movement * other_speed * delta_time;
     
-    g_model_matrix = glm::mat4(1.0f);
-    other_model_matrix= glm::mat4(1.0f);
-    g_model_matrix = glm::translate(g_model_matrix, player_position);
-    
+    g_model_matrix = glm::translate(g_model_matrix, glm::vec3(TRAN_VALUE, TRAN_VALUE, 0.0f));
+    g_model_matrix = glm::rotate(g_model_matrix, ROT_ANGLE, glm::vec3(0.0f, 0.0f, 1.0f));
+   
+
     g_angle += ROT_SPEED;
-        
-    // 2. Calculate x,y-coordinates based on angle using trig
     g_x_coords = RADIUS * glm::cos(g_angle);
     g_y_coords = RADIUS * glm::sin(g_angle);
-        
-    // 3. Reset model matrix back to the origin, and apply translation based on step 2
-    other_model_matrix = g_model_matrix;
-    other_model_matrix = glm::translate(other_model_matrix, glm::vec3(g_x_coords, g_y_coords, 0.0f));
-    other_model_matrix = glm::rotate(other_model_matrix, ROT_ANGLE, glm::vec3(0.0f, 5.0f, 0.0f));
-    
-    other_model_matrix =glm::scale(other_model_matrix, scale_vector);
 
-   
+    other_model_matrix = glm::translate(g_model_matrix, glm::vec3(g_x_coords, g_y_coords, 0.0f));
+    
+
+
+
+
+
+
 }
 
 void draw_object(glm::mat4 &object_model_matrix, GLuint &object_texture_id)
@@ -212,7 +217,7 @@ void draw_object(glm::mat4 &object_model_matrix, GLuint &object_texture_id)
     g_program.set_model_matrix(object_model_matrix);
     glBindTexture(GL_TEXTURE_2D, object_texture_id);
     glDrawArrays(GL_TRIANGLES, 0, 6);
-    
+
 }
 void draw_object2(glm::mat4 &object_model_matrix, GLuint &object_texture_id)
 {
@@ -223,7 +228,7 @@ void draw_object2(glm::mat4 &object_model_matrix, GLuint &object_texture_id)
 
 void render() {
     glClear(GL_COLOR_BUFFER_BIT);
-    
+
     // Vertices
     float vertices[] = {
         -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f,  // triangle 1
@@ -235,21 +240,21 @@ void render() {
         0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,     // triangle 1
         0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f,     // triangle 2
     };
-    
+
     glVertexAttribPointer(g_program.get_position_attribute(), 2, GL_FLOAT, false, 0, vertices);
     glEnableVertexAttribArray(g_program.get_position_attribute());
-    
+
     glVertexAttribPointer(g_program.get_tex_coordinate_attribute(), 2, GL_FLOAT, false, 0, texture_coordinates);
     glEnableVertexAttribArray(g_program.get_tex_coordinate_attribute());
-    
+
     // Bind texture
     draw_object(g_model_matrix, player_texture_id);
     draw_object2(other_model_matrix, other_texture_id);
-    
+
     // We disable two attribute arrays now
     glDisableVertexAttribArray(g_program.get_position_attribute());
     glDisableVertexAttribArray(g_program.get_tex_coordinate_attribute());
-    
+
     SDL_GL_SwapWindow(display_window);
 }
 
@@ -259,14 +264,14 @@ void shutdown() { SDL_Quit(); }
 int main(int argc, char* argv[])
 {
     initialise();
-    
+
     while (game_is_running)
     {
         process_input();
         update();
         render();
     }
-    
+
     shutdown();
     return 0;
 }
